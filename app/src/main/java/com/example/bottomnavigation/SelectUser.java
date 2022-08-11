34b322724a468115fce8d6d67f1495f1bf135a9f
win10 <song_jie99@hotmail.com>
2022-08-11 17:20:44 +0800

    second

---
+++ app/src/main/java/com/example/bottomnavigation/SelectUser.java
@@ -0,0 +1,14 @@
+package com.example.bottomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_user);
+    }
+}
---
+++ app/src/main/java/com/example/bottomnavigation/Splash.java
@@ -0,0 +1,30 @@
+package com.example.bottomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,Login.class));
+                finish();
+            }
+        },5500);
+
+
+    }
+}
