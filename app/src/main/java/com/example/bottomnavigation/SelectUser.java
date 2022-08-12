64029aa3d1ece7c4e67f2057bf9ae5c459b083e0
win10 <song_jie99@hotmail.com>
2022-08-12 15:06:22 +0800

    gambatehhh!!

--- app/src/main/java/com/example/bottomnavigation/SelectUser.java
+++ app/src/main/java/com/example/bottomnavigation/SelectUser.java
@@ -2,13 +2,40 @@ package com.example.bottomnavigation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SelectUser extends AppCompatActivity {
+
+    Button elderlyBtn, familyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_user);
+
+        elderlyBtn = findViewById(R.id.elderly);
+        familyBtn = findViewById(R.id.family);
+
+        elderlyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SelectUser.this, Login.class));
+                Toast.makeText(SelectUser.this, "Welcome", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        familyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SelectUser.this, Login2.class));
+                Toast.makeText(SelectUser.this, "Welcome", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
